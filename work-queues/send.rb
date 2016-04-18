729bee950fe06c646c3803a7376195310d8ec246
saulocamposi <saulocamposi@gmail.com>
2016-04-18 19:40:23 -0300

    Add workqueues send

---
+++ work-queues/send.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require "bunny"
+
+conn = Bunny.new
+conn.start
+
+ch   = conn.create_channel
+q    = ch.queue("hello")
+
+msg  = ARGV.empty? ? "Hello World!" : ARGV.join(" ")
+
+q.publish(msg, :persistent => true)
+puts " [x] Sent #{msg}"
+
+conn.close
